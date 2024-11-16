d89786c9a04fa832b37453ce24b7dcccae4d7e81
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-11-16 17:06:17 +0300

    added some models

--- lyrics/models.py
+++ lyrics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class songs(models.Model):
+    title = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    lyrics = models.TextField()
+
+    objects = models.DjongoManager()
